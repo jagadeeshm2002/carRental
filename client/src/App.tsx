e095bd3f05c378c3ecf2bab0b6c664add4c1b1b0
jagadeesh m <jagadeeshm2002@gmail.com>
2025-02-25 22:26:58 +0530

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+
+// import { Outlet } from 'react-router-dom';
+
+
+import LandingPage from './pages/LandingPage';
+// import NavBar from './components/navBar';
+
+// const CustomLayout = () => <>
+//   <NavBar />
+//   <Outlet /></>;
+
+function App() {
+
+
+  return (
+    <div className='font-Geraldton'>
+      <LandingPage /> </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/LandingPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+const LandingPage: React.FC = () => {
+    return (
+        <div className='min-h-screen w-full px-[2vw] md:px-[5vw] lg:px-[10vw] xl:px-[15vw] transition-all duration-50 ease-linear '>
+
+            <div className='bg-red-500 w-full h-screen'>
+                <header>
+                    <div>
+                        <p className='font-extrabold text-blue-700'>RENTCARS</p>
+                    </div>
+                </header>
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
