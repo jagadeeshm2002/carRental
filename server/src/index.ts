9c190ee682641efa4e0246b069f7f949ee830a07
jagadeesh m <91142244+jagadeeshm2002@users.noreply.github.com>
2025-03-25 23:15:34 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ import router from "./routers/index";
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin:  ["http://localhost:5173", "https://dl6ecdmuyksf1.cloudfront.net"],, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
